438d8f1bdd0d09121c6cfc05ae1be35e7de22018
Isabela Nunes <isabelanunes533905@gmail.com>
2024-04-22 10:35:49 -0300

    alterando cores do modo dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,19 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: { //manter a configuração existente e ao mesmo tempo adicionar elementos novos.
+        colors: {
+            dark: {
+                100: 'rgb(18, 18, 18)',
+                200: 'rgb(51, 51, 51)'
+            },
+            alura: {
+                100: '#167BF7',
+                200: '#051933'
+            }
+        }
+    },
+},
   plugins: [],
 }
 
