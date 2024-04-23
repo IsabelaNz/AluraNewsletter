4e0a59be5a89d1e018d2b44cf62de6971a9f5a55
Isabela Nunes <isabelanunes533905@gmail.com>
2024-04-23 20:40:57 -0300

    article

--- src/componentes/Article/Article.jsx
+++ src/componentes/Article/Article.jsx
@@ -7,6 +7,7 @@ const Article = ({ title, text, tags, image }) => {
                 text.map((content, index) => <p key={index} className="text-alura-200 dark:text-gray-400">{content}</p>)
             }
         </div>
+        { image && <img src={image} /> }
     </div>
 }
 
